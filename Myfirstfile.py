b3890775dd805119959c136cc95e81b79c1856d9
neeraj1144 <115168611+neeraj1144@users.noreply.github.com>
2022-10-06 20:19:38 +0530

    Update and rename README.md to Myfirstfile.py
    Adding a new file

---
+++ Myfirstfile.py
@@ -0,0 +1,6 @@
+# testrepo
+# Editing the file
+
+#Its a makedown file that create today
+
+Print("new python file")
--- README.md
+++
@@ -1,4 +0,0 @@
-# testrepo
-# Editing the file
-
-Its a makedown file that create today 
